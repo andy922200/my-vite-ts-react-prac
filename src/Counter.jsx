8ce129a22ed2b15a740ce082a47fea72c5a411d1
andy.lien <shaomin.lien@xrspace.io>
2022-06-09 18:17:16 +0800

    1. init project

---
+++ src/Counter.jsx
@@ -0,0 +1,11 @@
+const Counter = () => {
+    return (
+        <div className="container">
+            <div className="chevron chevron-up"></div>
+            <div className="number">12345</div>
+            <div className="chevron chevron-down"></div>
+        </div>
+    )
+}
+
+export default Counter
